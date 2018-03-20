b1266fea953019f9cb866d0f6546e5e112bda160
johna702 <johna702@student.liu.se>
2018-03-20 21:05:29 +0100

    add code for server and client (all of it)

--- my_pi_server.py
+++ my_pi_server.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import ir_sender, time, tv_alarm_script, datetime
+from threading import Timer
+
 
 app = Flask(__name__)
 app.debug = True
@@ -7,6 +10,56 @@ app.debug = True
 @app.route('/')
 def render_start_page():
     return render_template('index.html')
+
+
+@app.route('/remote_control', methods=['POST'])
+def remote_control():
+    button_request = request.get_json()['button_request']
+    ir_sender.send(button_request)
+
+
+timer = None
+
+@app.route('/alarm', methods=['POST'])
+def alarm():
+    alarm_request = request.get_json()['alarm_data']
+    if alarm_request != "clear alarm":
+        alarm_request_hour = int(alarm_request[0:2])
+        alarm_request_minute = int(alarm_request[3:5])
+        now = datetime.datetime.now()
+        year = now.year
+        month = now.month
+        day = now.day
+        hour = now.hour
+        minute = now.minute
+        second = now.second
+        current_time = datetime.datetime(year,month,day,hour,minute,second)
+        alarm_time = datetime.datetime(year, month, day, alarm_request_hour, alarm_request_minute, 0)
+        time_diff = (alarm_time-current_time).total_seconds()
+        if time_diff < 0:
+            seconds_in_a_day = 24*3600
+            time_diff = seconds_in_a_day + time_diff
+        print("time_diff = " + str(time_diff))
+        timer = Timer(time_diff, tv_alarm_script.run)
+        timer.start()
+        response = {
+            'status': "200",
+            'message': "Alarm set",
+            'data': ""
+        }
+        resp = jsonify(response)
+        resp.status_code = 200
+        return resp
+    else:
+        timer.cancel()
+        response = {
+            'status': "200",
+            'message': "Alarm stopped",
+            'data': ""
+        }
+        resp = jsonify(response)
+        resp.status_code = 200
+        return resp
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -1,14 +1,51 @@
 
 function setListeners() {
     var set_alarm_button = document.getElementById("set_alarm_button");
-    var alarm_clock_form = document.getElementById("alarm_clock_form");
+    var clear_alarm_button = document.getElementById("clear_alarm_button");
+    var set_alarm_form = document.getElementById("set_alarm_form");
 
     set_alarm_button.addEventListener("click", function () {
-        alert("NU GÅR LARMET!!!");
-        alarm_clock_form.setAttribute("onsubmit", "return false;");
+        var alarm_clock_value = document.getElementById("clock-picker").value;
+        alert(alarm_clock_value);
+        request = {url:"/alarm", data:alarm_clock_value};
+        sendRequest(request);
+        set_alarm_form.setAttribute("onsubmit", "return false;");
+    });
+
+    clear_alarm_button.addEventListener("click", function () {
+        alert("Alarm cleared");
+        request = {url:"/alarm", data:"clear alarm"};
+        sendRequest(request);
+        set_alarm_form.setAttribute("onsubmit", "return false;");
     });
 }
 
-window.onload = function (ev) {
+function sendRequest(request) {
+    var server_response = document.getElementById("server_response");
+
+    var request = new XMLHttpRequest();
+    request.open("POST", request.url, true);
+    var request_data = {alarm_data:request.data};
+
+    request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            var response = JSON.parse(this.responseText).message;
+            if (this.status === 200) {
+                server_response.style.display = "block";
+                server_response.textContent = response;
+            }
+
+            else if (this.status === 401) {
+                server_response.style.display = "block";
+                server_response.textContent = response;
+            }
+        }
+    };
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(request_data));
+
+}
+
+window.onload = function () {
     setListeners();
 };
